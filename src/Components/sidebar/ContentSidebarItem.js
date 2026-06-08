7a90ae7f21ef5251d3b5532c3c20b02ab9d1385d
Jagankumar <jagan.kumar@egovernments.org>
2026-06-08 15:01:48 +0530

    Updated with latest change

--- src/Components/sidebar/ContentSidebarItem.js
+++
@@ -1,9 +0,0 @@
-const ContentSidebarItem =[
-    {
-        id : 1,
-        item : 'Voice & Tone',
-        linkpath : "/ui-docs/content/voicetone"
-    },
-]
-
-export default ContentSidebarItem;
--- src/Components/sidebar/ResourcesSidebarItem.js
+++
@@ -1,9 +0,0 @@
-const ResourcesSidebarItem = [
-  {
-    id: 1,
-    item: "Resources",
-    linkpath: "/ui-docs/resources/resource",
-  },
-];
-
-export default ResourcesSidebarItem;
